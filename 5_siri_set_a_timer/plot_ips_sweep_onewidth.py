0bbb81015060e2233ef80a1406f4e2ec7dd3805a
Aedan Cullen <aedan@aedancullen.com>
2024-12-16 20:26:53 -0800

    Finish plot

--- 5_siri_set_a_timer/plot_ips_sweep_onewidth.py
+++ 5_siri_set_a_timer/plot_ips_sweep_onewidth.py
@@ -5,24 +5,26 @@ import sys
 import pickle
 import struct
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 FIELD_KEYS = []
 FIELD_IDXS = []
 FIELD_MASKS = []
 
 for i in range(64):
-    FIELD_KEYS.append(f"0x{i*4:03x}: SW_LOCK{i}")
+    FIELD_KEYS.append(f"0x{i*4:03x}:    SW_LOCK{i}")
     FIELD_IDXS.append(i*4)
     FIELD_MASKS.append(0xffffffff)
 
 for i in range(8):
-    FIELD_KEYS.append("0x148: CRITICAL[{i}]")
+    FIELD_KEYS.append(f"0x148:    CRITICAL[{i}]")
     FIELD_IDXS.append(64*4)
     FIELD_MASKS.append(0b1 << i)
 
 for i in range(8):
-    FIELD_KEYS.append(f"0x14c: KEY_VALID[{i}]")
+    FIELD_KEYS.append(f"0x14c:    KEY_VALID[{i}]")
     FIELD_IDXS.append(65*4)
     FIELD_MASKS.append(0b1 << i)
 
@@ -32,8 +34,7 @@ with open(sys.argv[1], "rb") as fh:
 GOOD_DUMP = regdumps[0][0] # ewww, assuming first is perfect
 
 x = np.asarray(delays)
-y = np.asarray(FIELD_KEYS)#np.arange(len(FIELD_KEYS))
-#X, Y = np.meshgrid(x, y)
+y = np.asarray(FIELD_KEYS)
 
 z_not_up = np.zeros(shape=(y.shape[0], x.shape[0]))
 z_dump_failed = np.zeros(shape=(y.shape[0], x.shape[0]))
@@ -64,8 +65,30 @@ for i, delay in enumerate(delays):
         prob_field_modified = n_field_modified / len(regdumps[0]) # ewww, assuming first is perfect
         z_field_modified[j, i] = prob_field_modified
 
-print(z_field_modified)
+plt.rcParams['axes.spines.left'] = False
+plt.rcParams['axes.spines.right'] = False
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['axes.spines.bottom'] = False
+
+plt.style.use('dark_background')
+
+cm1 = mpl.colors.LinearSegmentedColormap.from_list("", [(1, 0, 0, 0), (1, 0, 0, 1)])
+cm2 = mpl.colors.LinearSegmentedColormap.from_list("", [(0, 1, 0, 0), (0, 1, 0, 1)])
+cm3 = mpl.colors.LinearSegmentedColormap.from_list("", [(0, 0, 1, 0), (0, 0, 1, 1)])
 
 fig, ax = plt.subplots()
-ax.pcolormesh(x, y, z_field_modified, shading="nearest")
-plt.savefig("test.png")
+ax.set_axisbelow(True)
+ax.pcolormesh(x, y, z_not_up, shading="nearest", cmap=cm1)
+ax.pcolormesh(x, y, z_dump_failed, shading="nearest", cmap=cm2)
+ax.pcolormesh(x, y, z_field_modified, shading="nearest", cmap=cm3)
+ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
+ax.set_yticks([x+0.5 for x in ax.get_yticks()[:-1]], minor=True)
+ax.grid(axis="y", which="minor", color=(0.2, 0.2, 0.2))
+ax.tick_params(axis="y", length=0)
+ax.tick_params(axis="y", which="minor", length=0)
+ax.tick_params(axis="x", length=10)
+ax.tick_params(axis="x", which="minor", length=5)
+
+plt.gca().invert_yaxis()
+plt.show()
